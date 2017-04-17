3b2fbd3707d57d78e464e4fd189bc8bb781bd18e
rochefort <terasawan@gmail.com>
2017-04-17 21:28:56 +0900

    Init

---
+++ spec/atom-test-switcher-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTestSwitcherView from '../lib/atom-test-switcher-view';
+
+describe('AtomTestSwitcherView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
